54069c6c8dcb5866ba65a7142ea5944ca0b115b5
harishprashar <137916792+harishprashar@users.noreply.github.com>
2023-10-27 13:22:35 +0530

    Add files via upload

--- JavaScript Fundamentals - 1/1.js
+++ JavaScript Fundamentals - 1/1.js
@@ -1,1 +1,6 @@
+let totalValue = 2000;
+let discountPercentage = 20;
+let discountedValue = totalValue - (discountPercentage/100) * totalValue;
+
+console.log("The final price is: Rs." + discountedValue);
 
